fc8e6fe0aaeb33d860236e8e68a0ee7cddf338f3
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-02-02 15:13:08 +0300

    recursive binary search - js

--- JavaScript/0-binary_search.js
+++ JavaScript/0-binary_search.js
@@ -4,10 +4,8 @@ const arr = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91];
 const targ = 38;
 
 
-function iterative_binary_search(arr, targ) {
+function iterative_binary_search(arr, targ, l, r) {
     let m;
-    let l = 0;
-    let r = arr.length - 1;
 
     while (l <= r) {
         m = parseInt((l + r) / 2);
@@ -25,8 +23,30 @@ function iterative_binary_search(arr, targ) {
     return -1;
 }
 
-const idx = iterative_binary_search(arr, targ);
-if (idx == -1)
+function recursive_binary_search(arr, targ, l, r) {
+    const m = Math.floor((l + r) - 1)
+
+    if (arr[m] == targ)
+        return m;
+    else if (arr[m] < targ) {
+        l = m + 1;
+        return recursive_binary_search(arr, targ, l, r);
+    }
+    else if (arr[m] > targ) {
+        r = m - 1;
+        return recursive_binary_search(arr, targ, l, r);
+    }
+    else
+        return -1;
+}
+
+let l = 0;
+let r = arr.length - 1;
+
+const idx = iterative_binary_search(arr, targ, l, r);
+const ridx = recursive_binary_search(arr, targ, l, r);
+
+if (idx == -1 || ridx == -1)
     console.log(404);
 else
-    console.log("Found at ", idx);
+    console.log("Found at ", idx, ridx);
